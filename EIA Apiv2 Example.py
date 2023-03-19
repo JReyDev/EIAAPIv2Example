a444bc69ff8d002bb2694da779277ce2efe6a820
JReyDev <102834451+JReyDev@users.noreply.github.com>
2023-03-18 19:47:50 -0700

    Add files via upload

---
+++ EIA Apiv2 Example.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+
+def eiav2_api(series):
+    v2_api = 'pgXIEzLLBayRV56x25ZWqK50gDcGDMNvcSiCX5Qc'
+    parameters = f'&facets[series][]={series}&frequency=weekly&data[]=value&sort[0][column]=period&sort[0][direction]=asc'
+    url = 'https://api.eia.gov/v2/petroleum/sum/sndw/data/?api_key=' + v2_api + parameters
+    
+    response = requests.get(url)
+    json_data = response.json()
+    
+    if response.status_code == 200:
+            print ('Successful Connection!!!')
+    else:
+            print ('Error: Unable to connect')
+
+    data_df = pd.DataFrame(json_data['response']['data'], columns=['period','value'])
+    data_df.rename(columns={'period':'Date'},inplace=True)
+
+    data_df.to_csv(json_data['response']['data'][0]['series-description'] + ' Weekly' + '.csv', encoding = 'utf-8', index=False)
+
+eiav2_api('WCRSTUS1')
