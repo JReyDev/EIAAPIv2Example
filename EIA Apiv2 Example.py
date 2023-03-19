9565fd5cf2b40b359f381e1f49c7c680e40b07f3
JReyDev <102834451+JReyDev@users.noreply.github.com>
2023-03-18 19:50:20 -0700

    Update EIA Apiv2 Example.py

--- EIA Apiv2 Example.py
+++ EIA Apiv2 Example.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 
 def eiav2_api(series):
-    v2_api = 'pgXIEzLLBayRV56x25ZWqK50gDcGDMNvcSiCX5Qc'
+    v2_api = 'YOUR_KEY_HERE'
     parameters = f'&facets[series][]={series}&frequency=weekly&data[]=value&sort[0][column]=period&sort[0][direction]=asc'
     url = 'https://api.eia.gov/v2/petroleum/sum/sndw/data/?api_key=' + v2_api + parameters
     
@@ -19,4 +19,5 @@ def eiav2_api(series):
 
     data_df.to_csv(json_data['response']['data'][0]['series-description'] + ' Weekly' + '.csv', encoding = 'utf-8', index=False)
 
+#This pulls the data for U.S. Ending Stocks of Crude Oil (Thousand Barrels) Weekly
 eiav2_api('WCRSTUS1')
